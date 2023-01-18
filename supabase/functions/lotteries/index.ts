0589260a32a32c4f32c3d1f25dd768187c2fcb9a
pkafel <3944981+pkafel@users.noreply.github.com>
2023-01-18 23:08:38 +0100

    Initial commit with basic edge functions

---
+++ supabase/functions/lotteries/index.ts
@@ -0,0 +1,47 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+serve(async (req) => {
+
+  const { url, method } = req
+
+  if(method == 'POST') {
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+
+    const { data: lotteriesData, error: lotteriesError } = await supabaseClient.from('lotteries').select('*')
+
+    if (lotteriesError) throw lotteriesError
+
+    console.log("From database raw: " + lotteriesData);
+    console.log("From database" + JSON.stringify(lotteriesData));
+
+    return new Response(
+      JSON.stringify(lotteriesData),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  } else {
+    return new Response(
+      "{}", { 
+        headers: { "Content-Type": "application/json" },
+        status: 405,
+    });
+  }
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
