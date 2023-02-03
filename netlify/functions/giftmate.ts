b31e4344710a895628f6ea317d414a07199d14ba
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-03 12:24:40 +0100

    Create an instance of Supabase client

--- netlify/functions/giftmate.ts
+++ netlify/functions/giftmate.ts
@@ -5,6 +5,7 @@ const {
     SUPABASE_SERVICE_API_KEY
 } = process.env;
 const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters
