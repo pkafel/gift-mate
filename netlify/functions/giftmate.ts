e50497642b6de951c97df030278bc4a4ba00056a
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-03 12:11:29 +0100

    Connect to Supabase API via client

--- netlify/functions/giftmate.ts
+++ netlify/functions/giftmate.ts
@@ -1,5 +1,12 @@
 import { Handler } from '@netlify/functions'
 
+require('dotenv').config();
+const {
+    DATABASE_URL,
+    SUPABASE_SERVICE_API_KEY
+} = process.env;
+const { createClient } = require('@supabase/supabase-js');
+
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters
 
