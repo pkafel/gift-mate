5e999d1a75e22a6487cb79e286bf10645abcca71
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-04 23:10:56 +0100

    Handling form and sending data to the server

--- netlify/web/main.js
+++ netlify/web/main.js
@@ -1,7 +1,16 @@
-document.querySelector('#giftmate-form').addEventListener('submit', function(event) {
-    event.preventDefault()
-    const data = new FormData(event.target);
+document.getElementById('giftmate-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+
+    const form = event.currentTarget;
+
+    const data = new FormData(form);
     const value = Object.fromEntries(data.entries());
 
-    alert(JSON.stringify(value))
+    const url = form.action;
+
+    const response = await fetch(url, { method: 'POST', body: JSON.stringify(value) })
+      .then(response => alert(JSON.stringify(response.json())));
+
+    alert(JSON.stringify(value));
   });
