3f57ea1c684350fb8efcbba91b79d2ae60c8f408
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-06 22:25:22 +0100

    Redirect after successful form submission

--- netlify/web/main.js
+++ netlify/web/main.js
@@ -9,8 +9,7 @@ document.getElementById('giftmate-form').addEventListener('submit', async functi
 
     const url = form.action;
 
-    const response = await fetch(url, { method: 'POST', body: JSON.stringify(value) })
-      .then(response => alert(JSON.stringify(response.json())));
-
-    alert(JSON.stringify(value));
-  });
+    fetch(url, { method: 'POST', body: JSON.stringify(value) })
+      .then((response) => window.location.replace("http://www.google.com"))
+      .catch((error) => alert(`We have an error ${error}`));
+});
