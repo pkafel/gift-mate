62ae840f4ca5bf35ad716aa3e063d1e5c44e00c0
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-10 15:31:34 +0100

    Better separation of functionality in Javascript file

--- netlify/web/main.js
+++ netlify/web/main.js
@@ -1,19 +1,52 @@
-document.getElementById('giftmate-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    event.stopImmediatePropagation();
+// Functions 
 
-    const form = event.currentTarget;
+async function onFormSubmit(event) {
+  event.preventDefault();
+  event.stopImmediatePropagation();
 
-    const data = new FormData(form);
-    const value = Object.fromEntries(data.entries());
+  const form = event.currentTarget;
 
-    const url = form.action;
+  const data = new FormData(form);
+  const value = Object.fromEntries(data.entries());
 
-    fetch(url, { method: 'POST', body: JSON.stringify(value) })
-      .then((response) => response.json())
-      .then((data) => {
-        alert(JSON.stringify(data));
-        window.location.replace(`#lotteries/${data.id}`);
-      })
-      .catch((error) => alert(`We have an error ${error}`));
-});
+  const url = form.action;
+
+  fetch(url, { method: 'POST', body: JSON.stringify(value) })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(JSON.stringify(data));
+      window.location.replace(`#lotteries/${data.id}`);
+      showLoteryPage();
+    })
+    .catch((error) => alert(`We have an error ${error}`));
+}
+
+function setStateofPageOnLoad(event) {
+  if (window.location.href.indexOf('#lotteries') > -1) {
+    showLoteryPage();
+  } else if(window.location.href.indexOf('#user') > -1) {
+    showUserPage();
+  } else {
+    showFormPage();
+  }
+}
+
+function showFormPage() {
+  $('#giftmate-summary-table-div').hide();
+  $('#giftmate-form-div').show();
+}
+
+function showLoteryPage() {
+  $('#giftmate-form-div').hide();
+  $('#giftmate-summary-table-div').show();
+}
+
+function showUserPage() {
+  // TBD
+}
+
+// Events - handler binding
+
+document.addEventListener("DOMContentLoaded", setStateofPageOnLoad);
+
+document.getElementById('giftmate-form').addEventListener('submit', onFormSubmit);
