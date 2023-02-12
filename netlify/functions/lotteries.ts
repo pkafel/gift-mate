39390974ebd04dd96e975adc8dfb7ce4e98cdac0
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-12 22:25:02 +0100

    Return list of participants

--- netlify/functions/lotteries.ts
+++ netlify/functions/lotteries.ts
@@ -15,40 +15,68 @@ interface GiftMateFormData {
   participants: string
 }
 
-export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  console.log({event});
+async function createLottery(body:string) {
+  const formData: GiftMateFormData = JSON.parse(body);
 
-  if(event.httpMethod == 'POST') {
-    const formData: GiftMateFormData = JSON.parse(event.body);
+  const { data, error } = await supabase.rpc('add_lottery_with_participants', 
+    {name_arg: formData.name, description_arg: formData.description, participants_arg: formData.participants});
 
-    const { data, error } = await supabase.rpc('add_lottery_with_participants', 
-      {name_arg: formData.name, description_arg: formData.description, participants_arg: formData.participants});
+  if(error) {
+    console.log({error});
+    throw error;
+  }
+
+  console.log(`Created lottery: ${data}`);
+
+  return {
+    statusCode: 201,
+    body: `{"id": "${data}"}`,
+  };
+}
+
+async function getLotteryParticipants(url:string) {
+  const urlParts = url.split("/");
+  const lotteryId = urlParts[urlParts.length - 1];
+
+  if(uuidRegex.test(lotteryId)) {
+    console.log(`Get lottery: ${lotteryId}`);
+
+    const { data, error } = await supabase.from('lotteries')
+      .select(`lottery_uuid, lottery_participants(name, nonce, viewed)`)
+      .eq('lottery_uuid', lotteryId);
 
     if(error) {
       console.log({error});
       throw error;
     }
 
-    console.log(`Created lottery: ${data}`);
-
-    return {
-      statusCode: 201,
-      body: `{"id": "${data}"}`,
-    };
-  } else if(event.httpMethod == 'GET') {
-    const urlParts = event.rawUrl.split("/");
-    if(uuidRegex.test(urlParts.slice(-1))) {
-      console.log(`Get lottery: ${urlParts.slice(-1)}`);
+    if(data.length === 0 || data[0].lottery_participants.length === 0) {
       return {
-        statusCode: 200,
-        body: `{"participants:[]"}`,
-      }
-    } else {
-      return {
-        statusCode: 400,
+        statusCode: 404,
         body: `{"participants:[]"}`,
       }
     }
+
+    console.log(data[0].lottery_participants);
+    return {
+      statusCode: 200,
+      body: `{"participants": ${data[0].lottery_participants}}`,
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: `{"participants:[]"}`,
+    }
+  }
+}
+
+export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  console.log({event});
+
+  if(event.httpMethod == 'POST') {
+    return createLottery(event.body);
+  } else if(event.httpMethod == 'GET') {
+    return getLotteryParticipants(event.rawUrl);
   } else {
     return {
       statusCode: 405,
