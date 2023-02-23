7be165dee112995a2a1b833001a20b4b3a05e310
pkafel <3944981+pkafel@users.noreply.github.com>
2023-02-23 22:27:02 +0100

    Change the structure and styling of the frontend

--- netlify/web/main.js
+++ netlify/web/main.js
@@ -1,8 +1,6 @@
 // Functions 
 
 const uuidRegex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i;
-const lotteryPath = /^#lotteries\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i;
-const userPath = /^#users\/[0-9a-zA-Z]*\b$/i;
 
 async function onFormSubmit(event) {
   event.preventDefault();
@@ -18,33 +16,29 @@ async function onFormSubmit(event) {
   fetch(url, { method: 'POST', body: JSON.stringify(value) })
     .then((response) => response.json())
     .then((data) => {
-      alert(JSON.stringify(data));
-      window.location.replace(`#lotteries/${data.id}`);
-      showLoteryPage();
+      // alert(JSON.stringify(data));
+      window.location.replace(`/Minimal-Summary.html?id=${data.id}`);
     })
     .catch((error) => alert(`We have an error ${error}`));
 }
 
 function setStateofPageOnLoad(event) {
-  const subdirectory = window.location.href.substring(window.location.href.indexOf("#"));
-  if (lotteryPath.test(subdirectory)) {
+  if (window.location.pathname == '/Minimal-Summary.html') {
     showLoteryPage();
-  } else if(userPath.test(subdirectory)) {
+  } else if(window.location.pathname == '/Minimal-User.html') {
     showUserPage();
-  } else {
+  } else if(window.location.pathname != '/Minimal-About.html'){
     showFormPage();
   }
 }
 
 function showFormPage() {
-  $('#giftmate-summary-table-div').hide();
-  $('#giftmate-form-div').show();
+  document.getElementById('giftmate-form').addEventListener('submit', onFormSubmit);
 }
 
 function showLoteryPage() {
-  $('#giftmate-form-div').hide();
-  $('#giftmate-summary-table-div').show();
-  const uuid = window.location.href.split("/").slice(-1);
+  const urlParams = new URLSearchParams(window.location.search);
+  const uuid = urlParams.get('id');
 
   fetch(`/.netlify/functions/lotteries/${uuid}`, { method: 'GET'})
     .then((response) => {
@@ -64,7 +58,7 @@ function showLoteryPage() {
         nameCell.appendChild(newText);
 
         const linkCell = newRow.insertCell();
-        var newText = document.createTextNode(participant.nonce);
+        var newText = document.createTextNode(`${window.location.origin}/Minimal-User.html?id=${participant.nonce}`);
         linkCell.appendChild(newText);
 
         const viewedCell = newRow.insertCell();
@@ -76,21 +70,18 @@ function showLoteryPage() {
 }
 
 function showUserPage() {
-  $('#giftmate-summary-table-div').hide();
-  $('#giftmate-form-div').hide();
+  const urlParams = new URLSearchParams(window.location.search);
+  const nonce = urlParams.get('id');
 
-  const nonce = window.location.href.split("/").slice(-1);
   fetch(`/.netlify/functions/users/${nonce}`, { method: 'GET'})
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      alert(JSON.stringify(data));
+      // alert(JSON.stringify(data));
     });
 }
 
 // Events - handler binding
 
 document.addEventListener("DOMContentLoaded", setStateofPageOnLoad);
-
-document.getElementById('giftmate-form').addEventListener('submit', onFormSubmit);
